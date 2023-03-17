1cb9b962bcd8bd4afa89ba46135d6a6e28d30e04
TAYBI <taybib4@gmail.com>
2023-03-17 16:18:18 -0700

    fixing logic and betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,6 @@
 #include <time.h>
 /**
  * Description: main - print if number is negative or positive or zero
- * 
  * Return: 0 if success
  */
 int main(void)
@@ -12,12 +11,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n==0)
+
+	if (n == 0)
 		printf("%d is zero\n", n);
-	if(n<0)
-                printf("%d is negative\n", n);
-	else
-                printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n < 0)
+		printf("%d is positive\n", n);
+
 	return (0);
 }
