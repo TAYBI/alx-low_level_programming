60dfbaa8325fdf543cb07ca9b512d4bc6aa8ca77
TAYBI <taybib4@gmail.com>
2023-03-20 06:18:53 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Description: main - print alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		printf("%d", number);
+	putchar('\n');
+
+	return (0);
+}
