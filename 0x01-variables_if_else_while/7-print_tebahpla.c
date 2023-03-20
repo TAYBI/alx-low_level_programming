31e8e7698aed038367968645d484fd23818b2693
TAYBI <taybib4@gmail.com>
2023-03-20 07:35:33 -0700

    printing letters backword

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Description: main - print alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
