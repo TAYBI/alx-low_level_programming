f81a7ab8aef5cceb41fc806fb50b3c5256e485ac
TAYBI <taybib4@gmail.com>
2023-03-20 04:26:12 -0700

    fixing betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-                putchar(c);
-        for (c = 'A'; c <= 'Z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-        putchar('\n');
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
