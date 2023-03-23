112ae3cbc2c04d152bf566feffeb13ae3c9312b4
TAYBI <taybib4@gmail.com>
2023-03-23 15:30:33 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * _isdigit - determine if digit
+ *
+ * @c: character to determine
+ *
+ * Return: 1 if its 0 if not
+ */
+int _isdigit(int c)
+{
+	if ('c' >= 0 && c <= '9')
+		return (1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * _supper - writes if uppercase
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
 int _isupper(int c);
+/**
+ * _isdigit - writes if digi
+ *
+ */
+int _isdigit(int c);
 #endif
