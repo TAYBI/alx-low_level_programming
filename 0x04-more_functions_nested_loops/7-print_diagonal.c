65615f0a376b38334c85a7aa80c9482eede7cebb
TAYBI <taybib4@gmail.com>
2023-03-23 16:25:09 -0700

    fixing code

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,24 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-
 	if (n <= 0)
 	{
-		_putchar('$');
 		_putchar('\n');
 	} else
 	{
+		int i, j;
+
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar('\\');
-			_putchar('$')'
 			_putchar('\n');
 		}
 	}
 }
+
