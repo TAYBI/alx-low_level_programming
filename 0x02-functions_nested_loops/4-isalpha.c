45c24c12ee9f03cb269cd2eb55ab6264dd13bd4f
TAYBI <taybib4@gmail.com>
2023-04-16 03:47:19 -0700

    fixing code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,6 +5,7 @@
  * Return: 1 if the character is an English character.
  * 0 if the character is not an English character.
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
