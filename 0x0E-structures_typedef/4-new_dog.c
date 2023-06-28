d8cab842febd053e645417ca75a3d9ede871b392
Bilal <taybib4@gmail.com>
2023-06-28 12:53:00 +0100

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-
 /**
  *new_dog - creates a new dog
  *@name: name of dog
@@ -9,7 +8,6 @@
  *@owner:owner of dog
  *Return: new dog struct
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
